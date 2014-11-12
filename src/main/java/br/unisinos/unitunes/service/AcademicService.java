72a5ebbc8f4f8c549616d9b5e47d4071acea0c26
Paulo Gräbin <plgrabin@gmail.com>
2014-11-11 23:32:14 -0200

    no message

--- src/main/java/br/unisinos/unitunes/service/AcademicService.java
+++ src/main/java/br/unisinos/unitunes/service/AcademicService.java
@@ -86,7 +86,8 @@ public class AcademicService implements IService<Academic> {
 //		ps.setDouble(5, entity.getBalance());
 //		ps.setBoolean(6, entity.isAdmin());
 //		ps.setBoolean(7, entity.isExcluded());
-		ps.setLong(8, entity.getId());			// WHERE CLAUSE
+//		ps.setLong(8, entity.getId());			// WHERE CLAUSE
+		ps.setLong(2, entity.getId());			// WHERE CLAUSE
 		
 
         affectedRows = ps.executeUpdate();
